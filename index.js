b5be9c65fbb7ece69977c434645ca77daf70f12e
amiamitswe <amiamitswe@gmail.com>
2022-04-12 00:28:07 +0600

    fetche parsedUrl, path, trimmeePath, method of request, readers on request, query string with url

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 
 // dependencies
 const http = require('http');
+const url = require('url');
 
 // app object - module scaffolding
 const app = {};
@@ -29,6 +30,24 @@ app.createServer = () => {
 
 // handle request and response
 app.handleReqRes = (req, res) => {
+    // request handling
+    // gat the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+
+    const path = parsedUrl.pathname;
+
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
+    const headerObj = req.headers;
+    const queryStringObj = parsedUrl.query;
+
+    // // some console for result
+    // console.log(parsedUrl);
+    // console.log(path);
+    // console.log(trimmedPath);
+    // console.log(method);
+    // console.log(headerObj);
+
     // response handle
     res.end('hello world !');
 };
