5dde968beeb30c714a632c59294800e5f408263c
amiamitswe <amiamitswe@gmail.com>
2022-07-26 02:25:00 +0600

    separe server and worker file and combine them

--- index.js
+++ index.js
@@ -1,62 +1,30 @@
 /*
- * Title: Uptime monitoring application
- * Description: Uptime monitoring application using row node
+ * Title: Project Initial file
+ * Description: start server and worker
  * Author: Amit Samadder (by sumit shaha)
  * Data: 11.04.2022
+ * update: 26.07.2022
  *
  */
 
 // dependencies
-const http = require('http');
-const { handleReqRes } = require('./helper/handleReqRes');
-const environment = require('./helper/environment');
-// const data = require('./lib/data');
-const { sendTwilioSms } = require('./helper/notifications');
+const server = require('./lib/server');
+const workers = require('./lib/worker');
 
 // app object - module scaffolding
 const app = {};
 
-// // TODO: will be removed
-sendTwilioSms('01917784210', 'hello test sms', (err) => {
-  console.log(err);
-});
+// initiate the server and worker
+app.init = () => {
+  // start the server
+  server.init();
 
-// // TODO: will be removed
-// data.create(
-//     'test',
-//     'newFile',
-//     { name: 'amit', age: 26, occupation: 'Software Engineer' },
-//     (err) => {
-//         console.log(err);
-//     }
-// );
-
-// // TODO: will be removed
-// data.read('test', 'newFile', (err, result) => {
-//     console.log(err, result);
-// });
-
-// // TODO: will be removed
-// data.update('test', 'newFile', { name: 'abir' }, (err) => {
-//     console.log(err);
-// });
-
-// // TODO: will be removed
-// data.delete('test', 'newFile', (err) => {
-//     console.log(err);
-// });
-
-// create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  // listening server
-  server.listen(environment.port, () => {
-    console.log(`listening on port ${environment.port}`);
-  });
+  // start the workers
+  workers.init();
 };
 
-// handle request and response
-app.handleReqRes = handleReqRes;
+// call the init
+app.init();
 
-// start the server
-app.createServer();
+// export default
+module.exports = app;
